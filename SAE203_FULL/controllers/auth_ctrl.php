ec9c15641909bd77e91c0d8f24357cc3182b25b1
9lamm9 <101925292+9lamm9@users.noreply.github.com>
2025-06-05 15:42:24 +0200

    Add files via upload

---
+++ SAE203_FULL/controllers/auth_ctrl.php
@@ -0,0 +1,33 @@
+<?php
+require_once('models/crud.php');
+require_once('models/conf.php');
+
+function login_ctrl() {
+    $login = $_POST['login'] ?? '';
+    $pass = $_POST['pass'] ?? '';
+    $uid = $prenom = $nom = '';
+
+    if (auth_ldap($login, $pass, $uid, $prenom, $nom)) {
+        session_start();
+        $_SESSION['login'] = $login;
+        $_SESSION['uid'] = $uid;
+        $_SESSION['prenom'] = $prenom;
+        $_SESSION['nom'] = $nom;
+
+        $stmt = $pdo->prepare("SELECT COUNT(*) FROM users WHERE uid = ?");
+        $stmt->execute([$uid]);
+        $exists = $stmt->fetchColumn();
+
+        if ($exists) {
+            header("Location: index.php");
+        } else {
+            header("Location: index.php?route=formulaire");
+        }
+        exit;
+    } else {
+        echo "<p>Login ou mot de passe incorrect.</p>";
+    }
+}
+
+
+
