9ff9b47e2e662479ffc1d75d2fd4004466183dfc
gatlinALLOHO* <allohogatlin2@gmail.com>
2025-06-06 13:48:21 +0200

    Update trajet_view.php

--- SAE203_FULL/views/trajet_view.php
+++ SAE203_FULL/views/trajet_view.php
@@ -3,10 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Trajets du jour</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="/dev/SAE_R203-main/SAE203_FULL/css/style.css">
 </head>
 <body>
     <h1>Trajets disponibles aujourd’hui</h1>
+
+    <?php
+    // Inclusion du contrôleur qui fournit la variable $trajets
+    require_once '/var/www/html/dev/SAE_R203-main/SAE203_FULL/controllers/trajet_controle.php';
+
+    // Fonction d'affichage des trajets
+    function affiche_trajets($trajets) {
+        foreach ($trajets as $trajet) {
+            echo "<tr>";
+            echo "<td>" . htmlspecialchars($trajet['heureDepart']) . "</td>";
+            echo "<td>" . htmlspecialchars($trajet['heureArrivee']) . "</td>";
+            echo "<td>" . htmlspecialchars($trajet['prenom_conducteur'] . ' ' . $trajet['nom_conducteur']) . "</td>";
+            echo "<td>" . htmlspecialchars($trajet['lieuDepart']) . "</td>";
+            echo "<td>" . htmlspecialchars($trajet['points_intermediaires'] ?? 'Aucun') . "</td>";
+            echo "<td>" . htmlspecialchars($trajet['nbr_place'] ?? '') . "</td>";
+            echo "</tr>";
+        }
+    }
+    ?>
+
     <table border="1">
         <thead>
             <tr>
@@ -19,16 +39,13 @@
             </tr>
         </thead>
         <tbody>
-        <?php foreach ($trajets as $trajet): ?>
-            <tr>
-                <td><?= htmlspecialchars($trajet['heure_depart']) ?></td>
-                <td><?= htmlspecialchars($trajet['heure_arrivee']) ?></td>
-                <td><?= htmlspecialchars($trajet['prenom_conducteur'] . ' ' . $trajet['nom_conducteur']) ?></td>
-                <td><?= htmlspecialchars($trajet['lieu_depart']) ?></td>
-                <td><?= htmlspecialchars($trajet['points_intermediaires']) ?></td>
-                <td><?= htmlspecialchars($trajet['places_disponibles']) ?></td>
-            </tr>
-        <?php endforeach; ?>
+            <?php
+            if (isset($trajets) && is_array($trajets) && count($trajets) > 0) {
+                affiche_trajets($trajets);
+            } else {
+                echo '<tr><td colspan="6">Aucun trajet disponible pour aujourd’hui.</td></tr>';
+            }
+            ?>
         </tbody>
     </table>
 </body>
